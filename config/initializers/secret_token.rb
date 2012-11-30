d4680c44afdbb3cb35b58b09f270178f0286a95d
Subrat Rout <subratnrout@gmail.com>
2012-11-29 20:09:46 -0800

    Created a rails app and added stats update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = 'fc06f526de0c72595052ea199b39353f232b197c043f81c4872a48531601017cbac929859d73e25e4bb43ebc1ba494330319155befdd638179ba0c7dbd6bb915'
